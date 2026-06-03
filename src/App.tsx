7b1980fcd04d764c5366aa3e86ff9f5baa1f62cb
Jean-Phi Baconnais <jeanphilippe.baconnais@gmail.com>
2026-06-03 13:51:30 +0200

    :bug: Fix App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
 
